629b5d7d2d388fc2ee65e9dc22abdac5f49339fb
Enzo <enzorodrigues164@g,ail.com>
2024-12-29 13:04:14 -0300

    closes #4 - operações matemáticas

--- calc.js
+++ calc.js
@@ -1,8 +1,21 @@
-function calc() {
+function calc(op) {
     var n1 = parseFloat(document.getElementById("n1").value);
     var n2 = parseFloat(document.getElementById("n2").value);
+    var result = 0;
 
-    var result = n1 + n2;
+    switch(op) {
+        case '+' : result = n1 + n2;
+        break;
+
+        case '-' : result = n1 - n2;
+        break;
+
+        case '*' : result = n1 * n2;
+        break;
+
+        case '/' : result = n1 / n2;
+        break;
+    }
 
     document.getElementById("result").value = result;
 
